4ad9eb44cca05e6549be4fc2c3f8720fd24125e1
Lizzeth Rosero <lizzethcarolain@gmail.com>
2023-12-05 11:10:46 -0500

    b

--- aplicacion/catalogo/routes.py
+++ aplicacion/catalogo/routes.py
@@ -1,10 +1,13 @@
-from flask import render_template
+from flask import render_template, abort
 from . import catalogo
 from aplicacion.database.consultasproductos import obtenerproductos #se importa la funcion productos que esta en archivo consu productos
 
 @catalogo.route('/catalogo')
 def catalogo():
-   #creacion de variable
-   productos=obtenerproductos()
-   print(productos)
-   return render_template('catalogo.html', productos=productos)# se envia el listado de productos q se obtiene d ela consulta al template
+   try:
+      #creacion de variable
+      productos=obtenerproductos()
+      print(productos)
+      return render_template('catalogo.html', productos=productos)# se envia el listado de productos q se obtiene d ela consulta al template
+   except:
+      abort(500)
--- aplicacion/errores/routes.py
+++ aplicacion/errores/routes.py
@@ -5,4 +5,8 @@ from . import errores
 @errores.app_errorhandler(404)
 def page_not_found(error):
     return (render_template("error.html", error="Página no encontrada..."),404)
+
+@errores.app_errorhandler(500)
+def page_not_found(error):
+    return (render_template("error.html", error="Error en el servidor..."),500)    
     
